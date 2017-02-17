9091bd3cf1503b2018fd2480db65488017c3dd9b
Anna Walczak Prochowska <Anna Walczak Prochowska>
2017-02-17 18:19:29 +0100

    imagesUrlPoprawka

--- www/src/app/services/imagesUrl.js
+++ www/src/app/services/imagesUrl.js
@@ -341,7 +341,7 @@
                 stopaSrubydoPrzedstopiaOsteotomiaWeila:[
                     "images/stopa/srubyDoPrzestopia/osteotomiaWeila_25_snapOff.png",
                     "images/stopa/srubyDoPrzestopia/osteotomiaWeila_26_fixation.png",
-                ]
+                ],
                 stopaSrubyDoPrzedstopiaSruby:[
                     "images/stopa/srubyDoPrzestopia/srubyDoPrzedstopia_27_set.png",
                     "images/stopa/srubyDoPrzestopia/srubyDoPrzedstopia_28_range1.png",
